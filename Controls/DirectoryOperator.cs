1a9b704a7fdd8ec0ee648bab2c825b0578eda156
MicoriMatO <maksimvolhin08@gmail.com>
2023-07-06 23:46:46 +0700

    preLoadModDirectory

---
+++ Controls/DirectoryOperator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinecraftModsManager.Controls
+{
+    public class DirectoryOperator
+    {
+        public string ModDirectory { get; set; }
+        public List<string> GetInfoModDirectory()
+        {  
+            return GetDirectoryesInfo(ModDirectory);
+        }
+
+        private List<string> GetDirectoryesInfo(string directory)
+        {
+            List<string> inDirectory = new List<string>();
+            try
+            {
+                string[] directoryes = Directory.GetFiles(directory);
+                foreach (string dir in directoryes)
+                {
+                    inDirectory.Add(dir);
+                }
+            }
+            catch (Exception) { }
+            
+            return inDirectory;
+        }
+    }  
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MinecraftModsManager.Controls;
 
 namespace MinecraftModsManager
 {
@@ -20,9 +21,43 @@ namespace MinecraftModsManager
     /// </summary>
     public partial class MainWindow : Window
     {
+        private DirectoryOperator dirOper = new DirectoryOperator();
+
+        //list будет хранить только название мода
         public MainWindow()
         {
             InitializeComponent();
+
+            dirOper.ModDirectory = @"D:\Downloads\клиент";
+            PreLoadInfoDirectory();
         }
+
+        private void PreLoadInfoDirectory()
+        {
+            foreach (var item in dirOper.GetInfoModDirectory())
+            {
+                LoadMods.Items.Add(item);
+            }
+        }
+
+        private void ModOn_Click(object sender, RoutedEventArgs e)
+        {
+            SwitchList(LoadMods, ModsInGame);
+            // скопировать из пула модов в игру
+                // DirectoriOperator
+        }
+        private void ModOff_Click(object sender, RoutedEventArgs e)
+        {
+            SwitchList(ModsInGame, LoadMods);
+            //удалить из папки с игрой скопированые файлы
+        }
+
+        private void SwitchList(ListBox from, ListBox to)
+        {
+            to.Items.Add(from.SelectedItem);
+            from.Items.Remove(from.SelectedItem);
+        }
+
+        
     }
 }
